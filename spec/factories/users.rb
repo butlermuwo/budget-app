689fc5da131c36bc68ed7deb6dbb61d224c7fe84
unknown <butlermuwo@gmail.com>
2022-06-03 22:12:33 +0200

    Fix linter errors

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
-    factory :user do
-      name { Faker::Name.name }
-      email { Faker::Internet.email }
-      password { 'secret' }
-      password_confirmation { password }
-    end
+  factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { 'secret' }
+    password_confirmation { password }
   end
+end
--- spec/feature/category_add_spec.rb
+++ spec/feature/category_add_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Category add page', type: :feature do
-    before(:all) do
-        @user1 = create(:user)
-        @category1 = create(:category, user: @user1)
-        @payment1 = create(:payment, category_id: @category1.id, user_id: @user1.id)
+  before(:all) do
+    @user1 = create(:user)
+    @category1 = create(:category, user: @user1)
+    @payment1 = create(:payment, category_id: @category1.id, user_id: @user1.id)
+  end
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(@payment1).to be_valid
     end
-    
-    describe 'validations' do
-        it 'is valid with valid attributes' do
-        expect(@payment1).to be_valid
-        end
-    
-        it 'is not valid without a name' do
-        payment = build(:payment, name: nil)
-        expect(payment).to_not be_valid
-        end
-    
-        it 'is not valid without an amount' do
-        payment = build(:payment, amount: nil)
-        expect(payment).to_not be_valid
-        end
+
+    it 'is not valid without a name' do
+      payment = build(:payment, name: nil)
+      expect(payment).to_not be_valid
     end
+
+    it 'is not valid without an amount' do
+      payment = build(:payment, amount: nil)
+      expect(payment).to_not be_valid
+    end
+  end
 end
